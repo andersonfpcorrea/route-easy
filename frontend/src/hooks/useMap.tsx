708c8cc9f4d5fc2c39a6815dcbd2eaef1b8f4884
Anderson Correa <andersonfpcorrea@gmail.com>
2022-12-21 02:04:18 -0300

    feat: implement moving the map on marker click

--- frontend/src/hooks/useMap.tsx
+++ frontend/src/hooks/useMap.tsx
@@ -24,26 +24,42 @@ const LeafletThemes: ILeafletThemes = {
   },
 };
 
+const mapZoomLvl = 13;
+const defaultCoords: LatLngTuple = [-22.907, -43.173];
+
 export default function useMap(theme: IUseMapProps): void {
-  const defaultCoords: LatLngTuple = [-22.907, -43.173];
-  const { coords } = useContext(Context);
+  const { coords, tableData } = useContext(Context);
   const mapLocation =
     coords === null || coords.length === 0
       ? defaultCoords
       : coords[coords.length - 1].coords;
 
   useEffect(() => {
-    const map = L.map("map").setView(mapLocation, 13);
+    const map = L.map("map").setView(mapLocation, mapZoomLvl);
     L.tileLayer(LeafletThemes[theme].url, {
       maxZoom: LeafletThemes[theme].maxZoom,
       attribution: LeafletThemes[theme].attribution,
     }).addTo(map);
-    if (coords !== null) {
-      coords.forEach((c) => L.marker(c.coords).addTo(map));
+    if (coords !== null && tableData !== null && tableData.length > 0) {
+      coords.forEach((c, i) =>
+        L.marker(c.coords)
+          .on("click", () =>
+            map.setView(c.coords, mapZoomLvl, {
+              animate: false,
+            })
+          )
+          .addTo(map)
+          .bindPopup(
+            `${tableData[i]?.name as string} - ${
+              tableData[i]?.weigth as number
+            }kg`
+          )
+          .openPopup()
+      );
     }
 
     return () => {
       map.remove();
     };
-  }, [mapLocation, coords, theme]);
+  }, [mapLocation, coords, theme, tableData]);
 }
